91d03cc80c0c929c2f29425f0b6fbcf0a467c937
thquan0308 <tranhoangquanthq@gmail.com>
2024-10-05 01:00:55 +0700

    DESIGN HOMAPAGE part1 FE

--- src/services/userService.js
+++ src/services/userService.js
@@ -8,4 +8,28 @@ const getAllUsers = (inputId) => {
     //template string
     return axios.get(`/api/get-all-user?id=${inputId}`, { id: inputId })
 }
-export { handleLoginAPI, getAllUsers }
+
+const createNewUserService = (data) => {
+    console.log('check data from service: ', data)
+    return axios.post('/api/create-new-user', data)
+}
+
+const deleteUserService = (userId) => {
+    // return axios.delete('/api/delete-user', string{ id: userId })
+    return axios.delete('/api/delete-user', {
+        data: {
+            id: userId
+        }
+    });
+}
+
+const editUserService = (inputData) => {
+    return axios.put('/api/edit-user', inputData);
+}
+export {
+    handleLoginAPI,
+    getAllUsers,
+    createNewUserService,
+    deleteUserService,
+    editUserService
+}
