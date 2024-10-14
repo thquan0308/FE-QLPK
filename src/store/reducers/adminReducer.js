e52d879ed89da4db5223984abfdbcd147d974116
thquan0308 <tranhoangquanthq@gmail.com>
2024-10-14 23:44:00 +0700

    final form create user with redux FE

--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -8,30 +8,62 @@ const initContentOfConfirmModal = {
 }
 
 const initialState = {
+    isLoadingGender: false,
     genders: [],
     roles: [],
-    position: [],
+    positions: [],
 }
 
 const adminReducer = (state = initialState, action) => {
     switch (action.type) {
+        //GENDER
         case actionTypes.FETCH_GENDER_START:
-            console.log('fire fetch gender start: ', action)
-            return {
-                ...state,
-            }
-        case actionTypes.FETCH_GENDER_SUCCESS:
             let copyState = { ...state }
-            copyState.genders = action.data
-            console.log('fire fetch gender success: ', copyState)
+            copyState.isLoadingGender = true
             return {
                 ...copyState,
             }
-        case actionTypes.FETCH_GENDER_FAILED:
-            console.log('fire fetch gender failed: ', action)
+
+        case actionTypes.FETCH_GENDER_SUCCESS:
+            state.genders = action.data
+            state.isLoadingGender = false
             return {
                 ...state,
             }
+
+        case actionTypes.FETCH_GENDER_FAILED:
+            state.isLoadingGender = false
+            state.genders = []
+            return {
+                ...state,
+            }
+
+        //POSITION
+        case actionTypes.FETCH_POSITION_SUCCESS:
+            state.positions = action.data
+            return {
+                ...state,
+            }
+
+        case actionTypes.FETCH_POSITION_FAILED:
+            state.positions = []
+            return {
+                ...state,
+            }
+
+        //ROLE
+        case actionTypes.FETCH_ROLE_SUCCESS:
+            state.roles = action.data
+            return {
+                ...state,
+            }
+
+        case actionTypes.FETCH_ROLE_FAILED:
+            state.roles = []
+            return {
+                ...state,
+            }
+
         default:
             return state;
     }
